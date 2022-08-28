00d383ffeb652b6fedc7fe12e1a120ba825beb93
Jacek Kosiński <kashiash@gmail.com>
2022-08-28 08:52:35 +0200

    Initial Commit

---
+++ Shared/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Shared
+//
+//  Created by Jacek Kosinski U on 28/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
