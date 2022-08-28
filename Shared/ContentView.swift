b22743a178fb79bcd11685c1c558512401c43b1c
Jacek Kosiński <kashiash@gmail.com>
2022-08-28 10:41:20 +0200

    change bacgroundcolor

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,26 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0{
-        didSet {
-            print("New value is \(blurAmount)")
-        }
-    }
+    @State private var showConfirmation = false
+    @State private var backgroundColor = Color.white
+    
+    
     
     var body: some View {
-        VStack{
-            Text("Hello, World!")
-                    .blur(radius: blurAmount)
-
-                Slider(value: $blurAmount, in: 0...20)
-                .onChange(of: blurAmount){newValue in
-                    print("New value is \(newValue)")
-                }
-
-                Button("Random Blur") {
-                    blurAmount = Double.random(in: 0...20)
-                }
-        }
+        Text("Pies wam morde lizał")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showConfirmation = true
+            }
+            .confirmationDialog("Change bacground",isPresented: $showConfirmation){
+                Button("Red") {backgroundColor = .red}
+                Button("Green") {backgroundColor = .green}
+                Button("Blue") {backgroundColor = .blue}
+                Button("Teal") {backgroundColor = .teal}
+                Button("Reset") {backgroundColor = .white}
+                Button("Cancel") {}
+            } message:{
+                Text("Select a new color")
+            }
     }
 }
 
